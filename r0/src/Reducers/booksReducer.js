c77d62124282f0bcc07fb1a3bb6adbae049b8236
Lauris <91956740+Laurisdid@users.noreply.github.com>
2022-06-20 10:49:23 +0300

    push

---
+++ r0/src/Reducers/booksReducer.js
@@ -0,0 +1,49 @@
+function booksReducer(state, action) {
+  let newState;
+  switch (action.type) {
+    case "get_from_server":
+      newState = action.payload.map((b, i) => ({ ...b, row: i,show:true }));
+      break;
+    case "sortAZ":
+      newState = [...state].sort((a, b) => {
+        if (a.title > b.title) return 1;
+        if (b.title > a.title) return -1;
+        return 0;
+      });
+      break;
+    case "sort17":
+      newState = [...state].map((a) => a.price < 13 ? { ...a, show: true } : { ...a, show: false } );
+      break;
+    case "hide":
+   //   console.log(action.payload,'payload');
+      newState = [...state].map((b) => b.id === action.payload ? {...b, show: false} : {...b, show: true} )
+      break;
+    case "sortDEF":
+      newState = [...state].sort((a, b) => a.row - b.row );
+      break;
+      case "reset":
+      newState =[...state].map((a) => ({ ...a, show:true }));
+      break;
+      case "kaina":
+     //   console.log('payload', action.payload)
+        newState = [...state].map(o => o.price < action.payload ? {...o, show: true} : {...o, show: false} )
+      break;
+      case "sortPrice":
+      newState = [...state].sort((a, b) => {
+        if (a.price > b.price) return 1;
+        if (b.price > a.price) return -1;
+        return 0;
+      });
+      break;
+      case "reload":
+        newState = [...state].map((b, i) => ({ ...b, row: i,show:true }));
+        break;
+
+    default:
+      newState = [...state];
+  }
+
+  return newState;
+}
+
+export default booksReducer;
---
+++ r0/src/numbertocubes.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./App.scss";
+import rand from "./Functions/rand";
+// import { v4 as uuidv4 } from 'uuid'; https://docs.google.com/document/d/1m5Co9Lv-n7Mm3IXwsnhWmNaGvbRTImcpp52fCIjUHnk/edit
+
+function App() {
+    const [nr, setNr] = useState("");
+
+    const [array, setArray] = useState([]);
+
+    const click = () => {
+        const copyArray = [...array];
+        for (let i = 0; i < nr; i++) {
+            copyArray.push("");
+        }
+        setArray(copyArray);
+    };
+
+    return (
+                <div className="App">
+            <header className="App-header">
+                <fieldset>
+                    <input
+                        type="text"
+                        onChange={(e) => setNr(e.target.value)}
+                        value={nr}
+                    ></input>
+                    <button onClick={click}>Pridek kv</button>
+                </fieldset>
+                <div className="kvc">
+                    {array.map((a, i) => (
+                        <div className="kv" key={a}>
+                            {rand(100, 200)}
+                        </div>
+                    ))}
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default App;
