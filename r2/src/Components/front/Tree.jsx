038238eb98c59ac88b327a702af400ed75737f21
Lauris <laurisdid@gmail.com>
2022-06-28 14:52:34 +0300

    a

--- r2/src/Components/front/Tree.jsx
+++ r2/src/Components/front/Tree.jsx
@@ -4,14 +4,25 @@ import FrontContext from "./FrontContext";
 
 function Tree({ tree }) {
 
-    const { setCreateComment } = useContext(FrontContext);
+    const { setCreateComment, setRateNow } = useContext(FrontContext);
 
     const [com, setCom] = useState('');
+    
+    const [rate, setRate] = useState('5')
+    const rateIt = e =>{
+        setRate(e.target.value)
+        setRateNow({
+            rate:parseInt(e.target.value),
+            id:tree.id
+        });
+
+    }
 
     const handleComment = () => {
         setCreateComment({com, treeId: tree.id});
         setCom('');
     }
+
 
     return (
         <li className="list-group-item">
@@ -21,7 +32,20 @@ function Tree({ tree }) {
                     <span>{['Leaf', 'Spike', 'Palm'][tree.type - 1]}</span>
                     <i>{tree.height.toFixed(2)} m</i>
                     <u>{tree.good}</u>
+                    <i>
+                        {
+                            tree.rate_sum?'rate : '+(tree.rate_sum / tree.rates).toFixed(2):'no rates yet'
+                        }
+                    </i>
                 </div>
+                <div className="form-group mt-3">
+                    <label className="mr-2">rate It</label>
+                    <select value={rate} onChange={rateIt}>
+                        {
+                            [...Array(10)].map((_,i)=><option key={i} value={10-i}>{10-i}</option>)
+                        }
+                    </select>
+                    </div>
                 <ul className="list-group mt-2">
                     {
                     tree.coms ? tree.coms.slice(0, -5).split('-^o^-,').map((c, i) => <li className="list-group-item" key={i}>{c}</li>) : null
